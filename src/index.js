9edb283ee782d78ef8a38fac0d07ba08b2e0db83
anisdx1512-sys <anisdx1512@gmail.com>
2026-04-27 10:14:07 +0100

    Update sender name in email configuration

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ export default {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({
-            from: "horizonsdedjurdjura <onboarding@resend.dev>",
+            from: "Horizons Djurdjura Website <onboarding@resend.dev>",
             to: ["horizonsdedjurdjura15@gmail.com"],
             subject: `رسالة جديدة من: ${data.name}`,
             html: `
